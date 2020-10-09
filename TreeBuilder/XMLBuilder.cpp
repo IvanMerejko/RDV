c6d11797179bdf3f6142dd58e86f1757d560a84a
ivanm <mrt-ivan@ukr.net>
2020-10-09 09:07:15 +0300

    XML parser

--- TreeBuilder/XMLBuilder.cpp
+++ TreeBuilder/XMLBuilder.cpp
@@ -5,49 +5,60 @@
 #include "Types.h"
 #include <QDebug>
 #include <QXmlStreamReader>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
 namespace PDV::TreeBuilder
 {
 namespace
 {
-//NodePtr CreateNode(QXmlStreamReader& xml)
-//{
-
-//}
-
-}
-TreePtr XMLBuilder::Parse(std::string_view fileName)
+constexpr auto XMLAttribute = "<xmlattr>";
+const boost::property_tree::ptree& emptyPtree()
 {
-    QFile file(fileName.data());
-    if(!file.open(QFile::ReadOnly | QFile::Text))
-    {
-        qDebug() << "Cannot read file" << file.errorString();
-        return nullptr;
-    }
+    static boost::property_tree::ptree t;
+    return t;
+}
 
-    QXmlStreamReader xml(&file);
-    while(!xml.atEnd() && !xml.hasError())
+void ParseAttributtes(const boost::property_tree::ptree& tree)
+{
+   const auto& attributes = tree.get_child(XMLAttribute, emptyPtree());
+   for(const auto& [name, childs] : attributes)
+   {
+       const auto& value = attributes.get<std::string>(name);
+       qDebug() << name.c_str() << " " << value.c_str();
+   }
+}
+
+void ParseXMLElement(const boost::property_tree::ptree& tree)
+{
+    for(const auto& [name, childs] : tree)
     {
-        if(xml.readNext() == QXmlStreamReader::StartDocument)
+        if(name == XMLAttribute)
         {
+            ParseAttributtes(tree);
             continue;
         }
-
-        if(xml.isStartElement())
+        qDebug() << name.c_str();
+        for(const auto &[childName, childTree] : childs)
         {
-            qDebug() << "Start Name = " << xml.name() ;
-        }
-        else if(xml.isEndElement())
-        {
-            qDebug() << "End Name = " << xml.name() ;
-        }
-
-        qDebug() << "Name = " << xml.name() << " Value = " << xml.text();
-        qDebug() << "attributes";
-        for(const auto& attribute : xml.attributes())
-        {
-            qDebug() << "Name = " << attribute.name() << " Value = " << attribute.value().toString();
+            if(childName == XMLAttribute)
+            {
+                ParseAttributtes(childs);
+                continue;
+            }
+            qDebug() << childName.c_str();
+            ParseXMLElement(childTree);
         }
     }
+}
+
+}
+
+TreePtr XMLBuilder::Parse(std::string_view)
+{
+    boost::property_tree::ptree tree;
+    boost::property_tree::read_xml("/home/ivanm/PDV/file.xml", tree);
+    ParseXMLElement(tree);
     return nullptr;
 }
 
--- main.qml
+++ main.qml
@@ -1,6 +1,7 @@
 import QtQuick 2.12
 import QtQuick.Window 2.12
 import QtQuick.Controls 2.12
+import QtQuick.Dialogs 1.0
 import "componentCreation.js" as MyScript
 
 Window
@@ -10,16 +11,28 @@ Window
     width: 640
     height: 480
     title: qsTr("Hello World")
+//    Button
+//    {
+//        x: 39
+//        y: 49
+//        text: "add "
+//        font.family: "Arial"
+//        autoRepeat: false
+//        onClicked:
+//        {
+//            MyScript.createSpriteObjects("Block.qml");
+//        }
+//    }
     Button
     {
         x: 39
         y: 49
-        text: "add "
+        text: "load "
         font.family: "Arial"
         autoRepeat: false
         onClicked:
         {
-            MyScript.createSpriteObjects("Block.qml");
+            MyScript.createSpriteObjects("FileLoader.qml");
         }
     }
 
